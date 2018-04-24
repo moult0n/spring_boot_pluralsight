2852743b01ee533cfe76c30265c7d884b2babab3
Moulton <moulton0@gmail.com>
2018-04-24 13:17:59 -0700

    Finished up persistence database

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -3,11 +3,15 @@ package com.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hello world!
+ *
+ */
 @SpringBootApplication
-public class App{
-	public static void main(String[] args){
-
-		
-		SpringApplication.run(App.class, args);
-	}
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
 }
